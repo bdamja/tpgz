49c22f176f10d289081188fd1b73917dc9170937
madeline <qwertytrogi@gmail.com>
2026-09-21 00:38:04 -0700

    i hate cmake I HATE CMAKE I HATE CMAKE HOW IS THIS A THING

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,5 +1,8 @@
-if(NOT DEFINED TPGZ_VERSION)
-    set(TPGZ_VERSION "unknown")
+# Note: this is intentionally not named TPGZ_VERSION, which collides with the
+# <PROJECT-NAME>_VERSION variable CMake implicitly defines (and clears to empty,
+# since project() is called without VERSION) for a project named TPGZ.
+if(NOT DEFINED TPGZ_BUILD_VERSION)
+    set(TPGZ_BUILD_VERSION "unknown")
     find_package(Git QUIET)
     if(GIT_FOUND)
         execute_process(
@@ -10,7 +13,7 @@ if(NOT DEFINED TPGZ_VERSION)
             ERROR_QUIET
             RESULT_VARIABLE TPGZ_GIT_RESULT)
         if(TPGZ_GIT_RESULT EQUAL 0 AND TPGZ_GIT_HASH)
-            set(TPGZ_VERSION ${TPGZ_GIT_HASH})
+            set(TPGZ_BUILD_VERSION ${TPGZ_GIT_HASH})
             execute_process(
                 COMMAND ${GIT_EXECUTABLE} status --porcelain
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -18,9 +21,9 @@ if(NOT DEFINED TPGZ_VERSION)
                 OUTPUT_STRIP_TRAILING_WHITESPACE
                 ERROR_QUIET)
             if(TPGZ_GIT_DIRTY)
-                set(TPGZ_VERSION "${TPGZ_VERSION}-dirty")
+                set(TPGZ_BUILD_VERSION "${TPGZ_BUILD_VERSION}-dirty")
             endif()
         endif()
     endif()
 endif()
-message(STATUS "TPGZ_VERSION: ${TPGZ_VERSION}")
+message(STATUS "TPGZ_BUILD_VERSION: ${TPGZ_BUILD_VERSION}")
