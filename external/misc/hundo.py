df3fd379aaf2070b32978b62d00dbc062293e9da
bdamja <e>
2026-09-21 18:50:15 -0600

    metadata works

--- external/misc/hundo.py
+++ external/misc/hundo.py
@@ -326,7 +326,7 @@ def main(args=None):
     update_entry('dmt_poe', data = {
         'requirements': Requirements.POS,
         'pos': (-1221.2317, -1969.3409, 17314.1797),
-        'angle': 0#34005 
+        'angle': 34005 
     })
 
     update_entry('freezard_2', data = {
